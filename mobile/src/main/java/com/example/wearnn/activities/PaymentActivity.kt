005bfb88fa61ae8b03282fe14346d7ef23bf482e
Liviu Lupascu <Lupascu.Liviu@yahoo.com>
2024-04-14 20:18:11 +0300

    Finalizing discount section

---
+++ mobile/src/main/java/com/example/wearnn/activities/PaymentActivity.kt
@@ -0,0 +1,40 @@
+package com.example.wearnn.activities
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.wearnn.R
+import com.example.wearnn.data.model.PaymentInfo
+
+class PaymentActivity : AppCompatActivity(){
+
+//    member declaration
+    companion object {
+        const val euroSign : Char = '\u20AC'
+        const val percentage = "%"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.payment_activity);
+// Selecting the correct elements
+        val paymentAmountElement : TextView = findViewById(R.id.paymentValue) as TextView
+        val discountElement : TextView = findViewById(R.id.activityDiscountValue) as TextView
+        val remainingPaymentElement : TextView = findViewById(R.id.remainingPaymentValue) as TextView
+//capturing data
+        val paymentValue: String = paymentAmountElement.text.toString();
+        val discountPercentage: String = discountElement.text.toString();
+        val remainingValue: String = remainingPaymentElement.text.toString();
+//instantiating object class by converting to int
+        val info = PaymentInfo(paymentValue.toDouble(), discountPercentage.toDouble(), remainingValue.toDouble())
+//calculating discount
+        val discountAmount = info.paymentAmountValue * info.discountValue / 100
+        val newRemainingValue = info.paymentAmountValue - discountAmount
+//sending new values to activity
+        remainingPaymentElement.text = String.format("%.2f", newRemainingValue);
+
+
+        println("**************" + newRemainingValue)
+
+
+    }
+}
